f15a1aa3e78ef343238dd361de1eea95909ab4a7
arjun247778 <ag247778@gmail.com>
2024-04-01 12:48:53 +0530

    complete

--- app/page.js
+++ app/page.js
@@ -1,15 +1,39 @@
 "use client";
 import React,{useState} from "react";
 const page = () => {
-  const [title, settitle] = useState("")
-  const [desc, setdesc] = useState("")
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [maintask, setmaintask] = useState([]);
   const submitHandler = (e) =>{
      e.preventDefault()
-     console.log(title)
-     console.log(desc)
-     settitle("")
-     setdesc("")
+     setmaintask([...maintask, {title,desc}])
+     settitle("");
+     setdesc("");
+     console.log(maintask);
   }
+    let rendertask =<h2>No Task Available</h2>
+    const deletehandler= (i) => {
+      let copytask =[...maintask]
+      copytask.splice(i,1)
+      setmaintask(copytask)
+    }
+    if (maintask.length>0) {
+      rendertask = maintask.map((t ,i) => {
+        return (
+          <li key={i} className="flex items-center justify-between mb-8">
+            <div className="flex items-center justify-between w-2/3">
+              <h5 className="text-2xl font-semibold">{t.title}</h5>
+              <h6 className="text-lg font-semibold">{t.desc}</h6>
+            </div>
+            <button onClick={() => {
+            deletehandler(i)
+            }}
+             className="bg-red-500 rounded text-white font-bold px-4 py-2">Delete</button>
+          </li>
+        )
+      })
+      
+    }
   return (
     <>
       <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
@@ -42,6 +66,10 @@ const page = () => {
         Add Task
       </button>
       </form>
+      <hr/>
+      <div className="p-8 bg-slate-200">
+        <ul>{rendertask}</ul>
+      </div>
     </>
   );
 };
